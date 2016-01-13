5c39dab0b4a490adff3d789c1b618b54c76f378f
Jon Kolman <jonathankolman@gmail.com>
2016-01-13 16:13:58 -0500

    bet_stream

--- betstream/betstream/Streamer/bet_stream.py
+++ betstream/betstream/Streamer/bet_stream.py
@@ -151,7 +151,7 @@ class BetStream(object):
 		if sport not in available_sports:
 			print "skipping this bet based on sport"
 			return None
-		
+		as
 		home_team = " ".join(edgebet.home_team.split(",")).lower()
 		away_team = " ".join(edgebet.away_team.split(",")).lower()
 		for bet in Bovadabet.objects.filter(is_placed=False, home_team__icontains=edgebet.home_team, away_team__icontains=edgebet.away_team).order_by("-date_added"):
